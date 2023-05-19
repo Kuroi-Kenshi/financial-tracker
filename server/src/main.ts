a02c7eb070baacbbe0c430ff352324faf7e277b6
Alexander <quattro.ex@gmail.com>
2023-05-19 19:14:08 +0300

    configure server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  const config = app.get(ConfigService);
+  const appPort = config.get<number>('APP_PORT') || 3000;
+
+  await app.listen(appPort, () => {
+    console.log(`Server started on ${appPort} port`);
+  });
 }
 bootstrap();
