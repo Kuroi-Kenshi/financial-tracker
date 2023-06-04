3c27aaebeddf47f4811f21346eff8490e01fc235
Alexander <quattro.ex@gmail.com>
2023-06-04 16:04:34 +0300

    added global auth guard

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -1,9 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AtGuard } from 'src/guards';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
+    AuthModule,
+  ],
+  providers: [
+    //еще один способ задать глобальный Guard
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: AtGuard,
+    // },
   ],
 })
 export class AppModule {}
