c849ec7fa1109b1f8d3c9d1b85d68701a323c33f
Alexander <quattro.ex@gmail.com>
2023-06-11 09:38:43 +0300

    refactoring refreshToken strategy

--- server/src/modules/auth/strategies/rtJwt.strategy.ts
+++ server/src/modules/auth/strategies/rtJwt.strategy.ts
@@ -2,19 +2,13 @@ import { ConfigService } from '@nestjs/config';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/modules/prisma/prisma.service';
-import { User } from '@prisma/client';
 import { Request } from 'express';
 import { JwtPayload, JwtPayloadWithRt } from '../auth.types';
 
 @Injectable()
 export class RtJwtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-  constructor(
-    private configService: ConfigService,
-    private prisma: PrismaService,
-  ) {
+  constructor(private configService: ConfigService) {
     super({
-      // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       jwtFromRequest: ExtractJwt.fromExtractors([
         (req: Request) => {
           return req?.cookies?.Authentication;
@@ -26,12 +20,6 @@ export class RtJwtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   }
 
   validate(req: Request, payload: JwtPayload): JwtPayloadWithRt {
-    const refreshToken = req?.cookies?.Authentication;
-
-    if (!refreshToken) {
-      throw new ForbiddenException('Некорректный refresh token');
-    }
-
-    return { ...payload, refreshToken };
+    return { ...payload, refreshToken: req?.cookies?.Authentication };
   }
 }
