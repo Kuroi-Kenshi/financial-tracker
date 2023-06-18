2e9c3561533236b6a81c995e6c9443b6be44292e
Alexander <quattro.ex@gmail.com>
2023-06-18 23:45:07 +0300

    added expense seed data

--- server/prisma/seedData/Expense.ts
+++ server/prisma/seedData/Expense.ts
@@ -9,6 +9,21 @@ export const expenseCategory = [
     color: '#FFC300',
     userId: 1,
   },
+  {
+    name: 'Одежда',
+    color: '#FF1493',
+    userId: 1,
+  },
+  {
+    name: 'Домашние животные',
+    color: '#FF00FF',
+    userId: 1,
+  },
+  {
+    name: 'Коммунальные услуги',
+    color: '#FFA07A',
+    userId: 1,
+  },
 ];
 
 export const expense = [
@@ -27,5 +42,29 @@ export const expense = [
     userId: 1,
     categoryId: 2,
     currencyId: 4,
+  },
+  {
+    name: 'Футболка',
+    amount: 1500.0,
+    date: '2023-03-18T15:57:36.317Z',
+    userId: 1,
+    categoryId: 3,
+    currencyId: 4,
+  },
+  {
+    name: 'Корм для котов',
+    amount: 850.0,
+    date: '2023-06-18T15:57:36.317Z',
+    userId: 1,
+    categoryId: 4,
+    currencyId: 4,
+  },
+  {
+    name: 'Свет и газ',
+    amount: 2500.0,
+    date: '2023-04-18T15:57:36.317Z',
+    userId: 1,
+    categoryId: 4,
+    currencyId: 5,
   },
 ];
