707a25f4c95a4598b8165f5361f05e5650ed8b34
Alexander <quattro.ex@gmail.com>
2023-07-24 21:55:41 +0300

    test: add componentRender

---
+++ client/src/shared/libs/tests/componentRender.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
+import { StoreProvider } from '@/app/providers/StoreProvider';
+import { MantineProvider } from '@/app/providers/MantineProvider';
+import { StateSchema } from '@/shared/types/StateSchema';
+
+export interface componentRenderOptions {
+  route?: string;
+  initialState?: DeepPartial<StateSchema>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+}
+
+interface TestProviderProps {
+  children: ReactNode;
+  options?: componentRenderOptions;
+}
+
+export function TestProvider(props: TestProviderProps) {
+  const { children, options = {} } = props;
+  const { route = '/', initialState, asyncReducers } = options;
+
+  return (
+    <MemoryRouter initialEntries={[route]}>
+      <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
+        {children}
+      </StoreProvider>
+    </MemoryRouter>
+  );
+}
+
+export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+  return render(<TestProvider options={options}>{component}</TestProvider>);
+}
