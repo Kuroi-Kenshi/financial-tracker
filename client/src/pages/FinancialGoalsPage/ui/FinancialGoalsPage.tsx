cb1fbada8d77f1ed803a65c5f359a34be87aaf22
Alexander <quattro.ex@gmail.com>
2023-07-24 21:22:12 +0300

    feat: update FinancialGoalPage

--- client/src/pages/FinancialGoalsPage/ui/FinancialGoalsPage.tsx
+++ client/src/pages/FinancialGoalsPage/ui/FinancialGoalsPage.tsx
@@ -1,7 +1,12 @@
+import { FinancialGoalList } from '@/entities/FinancialGoals';
 import { Page } from '@/widgets/Page';
 
 const FinancialGoalsPage = () => {
-  return <Page>FinancialGoalsPage</Page>;
+  return (
+    <Page>
+      <FinancialGoalList withAddButton />
+    </Page>
+  );
 };
 
 export default FinancialGoalsPage;
