7ef5d2972909e76c11eea1bba8c5db5ceb02f5be
Alexander <quattro.ex@gmail.com>
2023-07-24 21:21:43 +0300

    feat: update ExpensePage

--- client/src/pages/ExpensePage/ui/ExpensesPage.tsx
+++ client/src/pages/ExpensePage/ui/ExpensesPage.tsx
@@ -1,132 +1,92 @@
-import { ExpenseList, getExpenses } from '@/entities/Expense';
+import { Expense, ExpenseList, ExpenseReqType, getFilteredExpenses } from '@/entities/Expense';
 import { ExpenseCategoryList } from '@/entities/ExpenseCategory';
-import { ExpenseEditForm } from '@/features/ExpenseEditForm';
-import { BarChart, DoughnutChart } from '@/shared/ui/Charts';
+import { getDayNumber } from '@/shared/libs/utils/date/date';
+import { BarChart, DoughnutChart, SvgLineChart } from '@/shared/ui/Charts';
+import { BarChartDataSet } from '@/shared/ui/Charts/BarChart/BarChart';
+import { ChartWidget, chartViewData } from '@/shared/ui/Charts/CustomSvgLineChart';
+import { DoughnutDataSet } from '@/shared/ui/Charts/DoughnutChart/DoughnutChart';
+import { ChartType, getBarChartDataSet, getCategoryDataSet } from '@/shared/ui/Charts/utils';
 import { Page } from '@/widgets/Page';
-import { Button, Drawer, Flex, Group, Loader } from '@mantine/core';
-import { Suspense, useEffect, useState } from 'react';
+import { Button, Drawer, Flex, Group } from '@mantine/core';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-// interface Expense {
-//   id: string;
-//   name: string;
-//   amount: number;
-//   date: string;
-//   categoryExpense: {
-//     id: number;
-//     color: string;
-//     name: string;
-//   };
-//   currency: {
-//     id: number;
-//     code: string;
-//     name: string;
-//     symbol: string;
-//   };
-// }
-
-interface ExpenseDataSet {
-  month: string;
-  total: number;
-}
-
-interface ExpenseCategoryDataSet {
-  category: string;
-  total: number;
-}
-
 const ExpensesPage = () => {
-  const expenses = useSelector(getExpenses);
-  const [expenseDataset, setExpenseDataset] = useState<ExpenseDataSet[]>([]);
-  const [expenseCategoryDataset, setExpenseCategoryDataset] = useState<ExpenseCategoryDataSet[]>(
-    []
-  );
-  const [addModalOpened, setAddModalOpened] = useState(false);
+  const expenses = useSelector(getFilteredExpenses);
+  const [expenseDataset, setExpenseDataset] = useState<BarChartDataSet>({
+    datasets: [],
+    labels: [],
+  });
+  const [expenseCategoryDataset, setExpenseCategoryDataset] = useState<DoughnutDataSet>({
+    datasets: [],
+    labels: [],
+  });
   const [categoryListOpened, setCategoryListOpened] = useState(false);
 
   useEffect(() => {
-    //@ts-ignore
-    const expenseDataSetTemp = expenses.reduce((acc, obj: Expense) => {
-      const monthName = new Intl.DateTimeFormat('ru-RU', { month: 'long' }).format(
-        new Date(obj.date)
-      ); //@ts-ignore
-      if (acc[monthName]) {
-        //@ts-ignore
-        acc[monthName].push(obj);
-      } else {
-        //@ts-ignore
-        acc[monthName] = [obj];
-      }
-      return acc;
+    const dataSet = getBarChartDataSet(expenses, 'Расходы', ChartType.MONTH);
+    const categoryDataSet = getCategoryDataSet(expenses, 'Расходы по категориям');
+
+    setExpenseDataset(dataSet);
+    setExpenseCategoryDataset(categoryDataSet);
+  }, [expenses]);
+
+  const prepareDataForChart = (expenses: Expense[]) => {
+    [
+      {
+        label: 0,
+        x: 0,
+        y: 334,
+      },
+      {
+        label: 1,
+        x: 1,
+        y: 235,
+      },
+    ];
+
+    const preparedExpenses = expenses.reduce((allEntity: {}, expense: Expense) => {
+      const dayNumber = getDayNumber(new Date(expense.date));
+      //@ts-ignore
+      if (allEntity[dayNumber]) allEntity[dayNumber] = allEntity[dayNumber] + expense.amount;
+      //@ts-ignore
+      allEntity[dayNumber] = expense.amount;
+
+      return allEntity;
     }, {});
 
-    const expenseDataSet = Object.entries(expenseDataSetTemp).map(([key, value]) => {
-      //@ts-ignore
-      const totalExpenseOfMonth = value.reduce((total: number, expense: Expense) => {
-        return (total += expense.amount);
-      }, 0);
-
+    const preparedDataForLineChart = Object.entries(preparedExpenses).map(([day, value]) => {
       return {
-        month: key,
-        total: totalExpenseOfMonth,
+        label: day,
+        x: day,
+        y: value,
       };
     });
+    preparedDataForLineChart.unshift({
+      label: '0',
+      x: '0',
+      y: 0,
+    });
 
-    //@ts-ignore
-    const expenseCategoryDataSetTemp = expenses.reduce((acc, obj: Expense) => {
-      //@ts-ignore
-      if (obj.categoryExpense?.name && acc[obj.categoryExpense?.name]) {
-        //@ts-ignore
-        acc[obj.categoryExpense.name].push(obj);
-      } else if (obj.categoryExpense?.name) {
-        //@ts-ignore
-        acc[obj.categoryExpense.name] = [obj];
-      }
-      return acc;
-    }, {});
-
-    const expenseCategoryDataSet = Object.entries(expenseCategoryDataSetTemp).map(
-      ([key, value]) => {
-        //@ts-ignore
-        const totalExpenseOfCategory = value.reduce((total: number, expense: Expense) => {
-          return (total += expense.amount);
-        }, 0);
-
-        return {
-          category: key,
-          total: totalExpenseOfCategory,
-        };
-      }
-    );
-
-    setExpenseDataset(expenseDataSet);
-    setExpenseCategoryDataset(expenseCategoryDataSet);
-  }, [expenses]);
+    return preparedDataForLineChart;
+  };
 
   return (
     <Page>
-      <Flex direction="column" mah="300px" gap="5px">
+      <Flex direction="column" gap="5px">
+        <ChartWidget chartViewData={chartViewData} />
+        <SvgLineChart data={prepareDataForChart(expenses)} />
         <Flex direction="row" mah="300px" gap="50px">
-          <BarChart dataset={expenseDataset} label="Траты за месяц" />
+          <BarChart dataset={expenseDataset} />
           <DoughnutChart dataset={expenseCategoryDataset} />
         </Flex>
         <Group mt="20px">
-          <Button color="indigo" onClick={() => setAddModalOpened(true)}>
-            Добавить
-          </Button>
           <Button color="cyan" onClick={() => setCategoryListOpened(true)}>
             Категории
           </Button>
         </Group>
-        <ExpenseList />
+        <ExpenseList mode={ExpenseReqType.NORMAL} withAddButton />
       </Flex>
-      <Suspense fallback={<Loader />}>
-        <ExpenseEditForm
-          opened={addModalOpened}
-          setOpened={() => setAddModalOpened(true)}
-          onClose={() => setAddModalOpened(false)}
-        />
-      </Suspense>
       <Drawer
         opened={categoryListOpened}
         onClose={() => setCategoryListOpened(false)}
