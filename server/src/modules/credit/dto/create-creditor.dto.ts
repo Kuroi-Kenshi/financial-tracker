5df7868b31c50dfb47be69471b0a166ba06e9c74
Alexander <quattro.ex@gmail.com>
2023-07-24 19:14:54 +0300

    feat: update credit dto

--- server/src/modules/credit/dto/create-creditor.dto.ts
+++ server/src/modules/credit/dto/create-creditor.dto.ts
@@ -3,12 +3,19 @@ import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateCreditDto {
   @ApiProperty()
+  name: string;
+
+  @ApiProperty()
   @IsOptional()
   description: string;
 
   @ApiProperty({ default: 1111 })
   @IsNumber()
   amount: number;
+
+  @ApiProperty({ default: '2023-05-16T19:20:30.451Z' })
+  @IsString()
+  startDate: string;
 
   @ApiProperty({ default: '2023-05-16T19:20:30.451Z' })
   @IsString()
